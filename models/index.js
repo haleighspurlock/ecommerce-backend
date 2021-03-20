8c1642ba14d2f51f8851f429df5ec6a93bf953a0
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-03-20 14:37:12 -0400

    complete

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
+// define category as having many products, thus creating foreign key in the product table.
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete:'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: '',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: '',
+});
 
 module.exports = {
   Product,
